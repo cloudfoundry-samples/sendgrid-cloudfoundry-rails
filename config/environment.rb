e5e3ce23e90371aa600458d76e32f6ff0154a245
notroot <notroot@ubuntu.(none)>
2013-05-30 15:54:03 +0000

    updated env settings

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,8 @@ ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
   :authentication => :plain,
-  :user_name      => 'azure_5aa22547fa814954329b6d5108672a9f@azure.com',
-  :password       => '2xthwczg',
-  :domain         => 'laur.example.com',
+  :user_name      => '',
+  :password       => '',
+  :domain         => 'example.com',
   :enable_starttls_auto => true
 }
